7859752dded43c05144a30743a0320ec131aa4b7
thraneh <thraneh@gmail.com>
2022-03-03 11:48:07 +0100

    catch2

--- test/main.cpp
+++ test/main.cpp
@@ -1,36 +1,9 @@
 /* Copyright (c) 2017-2022, Hans Erik Thrane */
 
-#include <gtest/gtest.h>
-#include <cstdlib>
+#define CATCH_CONFIG_RUNNER
 
-TEST(user_custom, test_1) {
-  const char *value = "";
-  char *end = nullptr;
-  auto result = std::strtoul(value, &end, 10);
-  EXPECT_EQ(result, decltype(result){0});
-  EXPECT_NE(end, nullptr);
-  EXPECT_EQ(*end, '\0');
-}
-
-TEST(user_custom, test_2) {
-  const char *value = "   123";
-  char *end = nullptr;
-  auto result = std::strtoul(value, &end, 10);
-  EXPECT_EQ(result, decltype(result){123});
-  EXPECT_NE(end, nullptr);
-  EXPECT_EQ(*end, '\0');
-}
-
-TEST(user_custom, test_3) {
-  const char *value = "   123:abc";
-  char *end = nullptr;
-  auto result = std::strtoul(value, &end, 10);
-  EXPECT_EQ(result, decltype(result){123});
-  EXPECT_NE(end, nullptr);
-  EXPECT_STREQ(end, ":abc");
-}
+#include <catch2/catch.hpp>
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  return Catch::Session().run(argc, argv);
 }
