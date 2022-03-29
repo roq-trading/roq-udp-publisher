a1a8548b3fcf417baebd2e6c1eefd0b950d2acd6
thraneh <thraneh@gmail.com>
2022-03-29 14:01:31 +0200

    refactor object caching

--- src/roq/pubsub/config.cpp
+++ src/roq/pubsub/config.cpp
@@ -43,9 +43,9 @@ void Config::dispatch(server::Config::Handler &handler) const {
       .mbp_min_trade_vol_multiplier = NaN,
       .mbp_allow_remove_non_existing = {},
       .mbp_allow_price_inversion = {},
-      .oms_request_id_type = server::RequestIdType::BASE64,
       .oms_download_has_state = {},
       .oms_download_has_routing_id = {},
+      .oms_request_id_type = RequestIdType::BASE64,
   };
   handler(settings);
   for (auto &iter : rate_limits)
