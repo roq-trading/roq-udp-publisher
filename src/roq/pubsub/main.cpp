1839f039b3e5df37b9342cd57b7b76490373a243
thraneh <thraneh@gmail.com>
2022-05-13 08:14:41 +0200

    log host

--- src/roq/pubsub/main.cpp
+++ src/roq/pubsub/main.cpp
@@ -15,6 +15,7 @@ int main(int argc, char **argv) {
              {
                  .description = DESCRIPTION,
                  .package_name = ROQ_PACKAGE_NAME,
+                 .host = ROQ_HOST,
                  .build_version = ROQ_BUILD_VERSION,
                  .build_number = ROQ_BUILD_NUMBER,
                  .build_type = ROQ_BUILD_TYPE,
