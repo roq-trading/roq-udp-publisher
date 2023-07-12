be192652ec431d16dcb0e3979fcb43630ebdf1e3
thraneh <thraneh@gmail.com>
2023-07-12 15:12:34 +0200

    chore

--- src/roq/udp_publisher/application.cpp
+++ src/roq/udp_publisher/application.cpp
@@ -11,19 +11,13 @@ using namespace std::literals;
 namespace roq {
 namespace udp_publisher {
 
-// === CONSTANTS ===
-
-namespace {
-auto const TYPE = server::Type::MARKET_DATA;
-}  // namespace
-
 // === IMPLEMENTATION ===
 
 int Application::main(args::Parser const &args) {
-  Settings settings{args, TYPE};
+  Settings settings{args};
   Config config{settings};
   auto context = server::create_io_context(settings);
-  server::Trading<Gateway>{settings, config, *context}.dispatch();
+  server::MarketData<Gateway>{settings, config, *context}.dispatch();
   return EXIT_SUCCESS;
 }
 
